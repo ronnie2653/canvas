c432792109ad8e0f1e7e460245cd2f915a8d1cd7
alex <ronnie.coleman.2653@gmail.com>
2016-01-23 23:57:31 +0100

    Added implicit conversion from String to Int.

--- src/main/scala/my/canvas/impl/package.scala
+++ src/main/scala/my/canvas/impl/package.scala
@@ -1,10 +1,8 @@
 package my.canvas
 
 /**
-*  Created by Sandor Nagy on 1/23/16.
-*/
-
-
+ * Created by Sandor Nagy on 1/23/16.
+ */
 package object impl {
-    implicit def fromString(s: String): Int = s.toInt
+  implicit def fromString(s: String): Int = s.toInt
 }
